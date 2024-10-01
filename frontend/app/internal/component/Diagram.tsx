f569ec4f860fd24576b1055bf854e9b803c582c2
Kirill Romanov <romanov.k48@gmail.com>
2024-10-01 12:53:44 +0700

    add opportunity move and create shape
    Signed-off-by: Kirill Romanov <romanov.k48@gmail.com>

---
+++ frontend/app/internal/component/Diagram.tsx
@@ -0,0 +1,27 @@
+import { useSvgContainer, updateSvg } from "../svgContainer/hook";
+import { SvgContainer } from "../svgContainer/SvgContainer";
+import { Svg } from "@svgdotjs/svg.js";
+
+export const Diagram = () => {
+    const { setHandle, svgContainer } = useSvgContainer();
+
+    const handleOnload = (svg: Svg, container: HTMLDivElement) => {
+        // Set svg dimensions 
+        svg.size(300, 300);
+    }
+
+    const clickHandler = updateSvg(svgContainer, svg => {
+        const circle = svg.circle(100);
+
+        circle.on('click', (e) => {
+            circle.move(Math.floor(Math.random() * 50), Math.floor(Math.random() * 50));
+        })
+    })
+
+    return (
+        <div className="app">
+            <SvgContainer setHandle={setHandle} width='300px' height='300px' margin='0 auto' onload={handleOnload}/>
+            <button onClick={clickHandler}>Add new shape</button>
+        </div>
+    );
+}
