c7cbf2d66e4c5f42c9bbb50f9a85f089a4cc4aa6
Александр Дмитриевич Метлин <83118071+MetlinAlexander@users.noreply.github.com>
2024-11-13 13:59:33 +0700

    shapes with toolbar (#10)
    * shapes with toolbar
    
    * Refactor shape constraints reuse
    
    * with bugs
    
    * make sub element
    
    Signed-off-by: Kirill Romanov <romanov.k48@gmail.com>
    
    * finish figures
    
    ---------
    
    Signed-off-by: Kirill Romanov <romanov.k48@gmail.com>
    Co-authored-by: metalexo2 <metalexo2@yandex.ru>
    Co-authored-by: Kirill Romanov <romanov.k48@gmail.com>

--- frontend/app/internal/component/tools/ToolBar.tsx
+++ frontend/app/internal/component/tools/ToolBar.tsx
@@ -1,32 +1,61 @@
 import { SvgContainerHandle, updateSvg } from "~/internal/svg/svgContainer/hook";
 import { Button } from "~/components/ui/button";
-import { Box, Circle} from "@svgdotjs/svg.js";
+import { Box } from "@svgdotjs/svg.js";
 import { SquarePlus } from "lucide-react";
-import { SvgShape } from "~/internal/svg/svgShape/svgShape";
 import { GeneralDraggable } from "~/internal/svg/svgShape/draggable/GeneralDraggable";
-import { ConstraintMovable } from "~/internal/svg/svgShape/movable/ConstraintMovable";
-import { GeneralMovable } from "~/internal/svg/svgShape/movable/GeneralMovable";
 import { UseCaseShape } from "~/internal/svg/svgShape/shapes/UseCaseShape";
-
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import {ShapeConfig, shapeConfigs} from "~/internal/svg/diagramShapes/SvgFigureConfigs"
 
 interface DiagramProps {
     svgContainer: SvgContainerHandle | undefined;
 }
 
-export const ToolBar = ({svgContainer}: DiagramProps) => {
-    const clickHandler = updateSvg(svgContainer, svg => {
-        const constraint = new Box(0, 0, 1080, 720)
-        const shape = new Circle({r: 50, cx: 100, cy: 100});
-        new UseCaseShape(shape, svg, constraint);
-    })
-
+export const ToolBar = ({ svgContainer }: DiagramProps) => {
     return (
         <div className="outline rounded-md ml-10">
             <div className="flex w-20 flex-col justify-center gap-10">
-                <Button onClick={clickHandler} title="add shape">
-                    <SquarePlus />
-                </Button>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button title="Add Shape">
+                            <SquarePlus />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        {shapeConfigs.map((config, idx) => (
+                            <DropdownMenuItem key={idx}>
+                                <ShapeDropDownMenu config={config} svgContainer={svgContainer!} />
+                            </DropdownMenuItem>
+                        ))}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
-    )
+    );
+};
+
+interface ShapeDropDownMenuProps {
+    config: ShapeConfig;
+    svgContainer: SvgContainerHandle;
+}
+
+const ShapeDropDownMenu = ({config, svgContainer}: ShapeDropDownMenuProps) => {
+
+    const clickHandler = updateSvg(svgContainer, () => {
+        const constraint = new Box(0, 0, 1080, 720);
+        const shape = config.createShape();
+        new UseCaseShape(shape, svgContainer.svg, constraint);
+    });
+
+    return (<>
+       <button className="h-full w-full flex flex-row gap-1" onClick={clickHandler}>
+            <config.icon className="mr-2 h-4 w-4" />
+            <div>{config.name}</div>
+        </button> 
+    </>)
 }
---
+++ frontend/app/internal/svg/diagramShapes/SvgFigureConfigs.ts
@@ -0,0 +1,44 @@
+import { Circle, Rect , Shape, Ellipse, Polyline, SVG } from "@svgdotjs/svg.js";
+import { Circle as CircleIcon, Square, RectangleHorizontal, Triangle, LucideProps, Unlink2 } from "lucide-react";
+
+export type ShapeConfig = {
+    name: string,
+    createShape: () => Shape
+    icon: React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>
+}
+
+export const shapeConfigs: ShapeConfig[] = [
+    {
+        name: "Circle",
+        icon: CircleIcon,
+        createShape: () => new Circle({ r: 50, cx: 100, cy: 100 }),
+    },
+    {
+        name: "Square",
+        icon: Square,
+        createShape: () => new Rect({ width: 90, height: 90, x: 100, y: 100 }),
+    },
+    {
+        name: "Rectangle",
+        icon: RectangleHorizontal,
+        createShape: () => new Rect({ width: 140, height: 90, x: 100, y: 100 }),
+    },
+    {
+        name: "Oval",
+        icon: Unlink2,
+        createShape: () => new Ellipse({ cx: 100, cy: 100, rx: 100, ry: 50 }),
+    },
+    {
+        name: "Triangle",
+        icon: Triangle,
+        createShape: () => {
+            const sideLength = 100;
+            const height = (Math.sqrt(3) / 2) * sideLength;
+            const x = 100;
+            const y = 100;
+            const points = `${x},${y} ${x - sideLength / 2},${y + height} ${x + sideLength / 2},${y + height}`;
+            var draw = SVG().addTo('body').size(300, 130);
+            return draw.polyline(points) as Shape;
+        },
+    },
+];
