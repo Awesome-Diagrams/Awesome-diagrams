b10fd2fdeee78837f6ac6038081da2d49ce34ec5
Igor Chaiko <i.chaiko@g.nsu.ru>
2024-12-24 16:36:49 +0700

    .

---
+++ Backend/src/main/kotlin/diagrams/repository/DiagramRepository.kt
@@ -0,0 +1,8 @@
+package diagrams.repository
+
+import diagrams.entity.Diagram
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface DiagramRepository : JpaRepository<Diagram, Long>
